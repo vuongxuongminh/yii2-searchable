a8d0f265a1020747be203bcb12533a99013950c8
Vuong Minh <vuongxuongminh@gmail.com>
2019-04-15 09:39:42 +0700

    Added console command support flush and import index data from models

---
+++ src/CommandController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @link https://github.com/vuongxuongminh/yii2-search
+ * @copyright Copyright (c) 2019 Vuong Xuong Minh
+ * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
+ */
+
+namespace vxm\search;
+
+use yii\console\Controller;
+
+/**
+ * Class CommandController support import and flush model classes
+ *
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class CommandController extends Controller
+{
+    /**
+     * @var string models class name separate by `,`
+     */
+    public $models = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function options($actionID)
+    {
+        return array_merge(parent::options($actionID), ['models']);
+    }
+
+    /**
+     * Import the given models into the search index
+     */
+    public function actionImport()
+    {
+        $models = explode(',', $this->models);
+        $models = array_filter($models);
+
+        foreach ($models as $model) {
+            $model = trim($model);
+            $model::makeAllSearchable();
+
+            $this->stdout('All [' . $model . '] records have been imported.');
+        }
+    }
+
+    /**
+     * Flush all of the models records from the index.
+     */
+    public function actionFlush()
+    {
+        $models = explode(',', $this->models);
+
+        foreach ($models as $model) {
+            $model = trim($model);
+            $model::deleteAllFromSearch();
+
+            $this->stdout('All [' . $model . '] records have been flushed.');
+        }
+    }
+
+
+
+}
